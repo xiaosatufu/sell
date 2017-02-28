06aa5be95403e6bf74f70c6088702546fadba137
xiaosatufu <xiaosatufu@163.com>
2017-02-28 17:48:21 +0800

    sell border1px边框

--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import VureRouter from 'vue-router';
+import App from './App';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import seller from 'components/seller/seller';
 
-/* eslint-disable no-new */
-new Vue({
-  el: 'body',
-  components: { App }
-})
+Vue.use(VureRouter);
+
+let app = Vue.extend(App);
+
+let router = new VureRouter({
+    linkActiveClass: 'active'
+});
+
+router.map({
+    '/goods': {
+        component: goods
+    },
+    '/ratings': {
+        component: ratings
+    },
+    '/seller': {
+        component: seller
+    }
+
+});
+
+router.start(app, '#app');
+router.go('/goods');
