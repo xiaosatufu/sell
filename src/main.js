836e18e4fd5990e198475d112fd6edac9d45d69a
xiaosatufu <xiaosatufu@163.com>
2017-03-01 17:31:50 +0800

    头部区域

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import VureRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import App from './App';
 import goods from 'components/goods/goods';
 import ratings from 'components/ratings/ratings';
 import seller from 'components/seller/seller';
-
+import 'common/stylus/index.styl';
 Vue.use(VureRouter);
+Vue.use(VueResource);
 
 let app = Vue.extend(App);
 
